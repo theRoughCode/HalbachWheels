ad24884655c64e2026a5bfe7ac28da4ae1b90c03
raphael <raph.koh@gmail.com>
2017-02-23 20:15:40 -0500

    Update screenshots

--- main.py
+++ main.py
@@ -23,21 +23,42 @@ r = ((side_len + spacing) * num_magnets) / (2 * np.pi)  # radius of wheel
 # CONVERSIONS
 rpm_to_angular_vel = lambda rpm: 30 * rpm / np.pi
 tangential_vel = lambda angular_vel: r * angular_vel    # tangential velocity of wheel given
+rpm_to_tan_vel = lambda rpm: tangential_vel(rpm_to_angular_vel(rpm))
 
-# FORCES
-F_lift = lambda v: ((3 * mu_0 * m ** 2) / (32 * np.pi * z_0 ** 4)) * (1 - (w / np.sqrt(v ** 2 + w ** 2)))
-F_drag = lambda v: (w / v) * F_lift(v)
+# FORCES (RPM -> Force)
+F_lift = lambda rpm: ((3 * mu_0 * m ** 2) / (32 * np.pi * z_0 ** 4)) * (1 - (w / np.sqrt(rpm_to_tan_vel(rpm) ** 2 + w ** 2)))
+F_drag = lambda rpm: (w / rpm_to_tan_vel(rpm)) * F_lift(rpm)
+
+def getPOI(f, g, x0):
+    tolerance = 0.0001
+    h = lambda x: f(x) - g(x)
+    dh = lambda x : (h(x+tolerance) - h(x)) / tolerance
+    x1 = x0 - h(x0)/dh(x0)
+    if h(x1) > tolerance:
+        return getPOI(f, g, x1)
+    else:
+        return x1
+
 
 # Compute the x and y coordinates for points on sine and cosine curves
 x = np.arange(0.001, 30)
 F_lift_plot = F_lift(x)
 F_drag_plot = F_drag(x)
+max_drag_x = np.argmax(F_drag_plot)
+max_drag_y = F_drag(max_drag_x)
+POI_x = getPOI(F_lift, F_drag, 2)
+POI_y = F_drag(POI_x)
 
 # Plot the points using matplotlib
 plt.plot(x, F_lift_plot)
 plt.plot(x, F_drag_plot)
+plt.plot(POI_x, POI_y, 'ro')
+plt.plot(max_drag_x, max_drag_y, 'bD')
 plt.xlabel('RPM (cycles/min)')
 plt.ylabel('Force (N)')
 plt.title('Force vs RPM')
 plt.legend(['Lift Force', 'Drag Force'])
+#plt.annotate('POI = (' + str('{0:.2f}'.format(POI_x)) + ', ' + str('{0:.2f}'.format(POI_y)) + ')', xy=(2, 1), xytext=(0.5, POI_y))
+#plt.annotate('Max F_drag = (' + str('{0:.2f}'.format(max_drag_x)) + ', ' + str('{0:.2f}'.format(max_drag_y)) + ')',
+ #            xy=(2, 1), xytext=(max_drag_x + 0.1, max_drag_y))
 plt.show()
