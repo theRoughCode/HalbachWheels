95e28ac6eb4ee308cb9517fd1713aaea0f76b9c0
raphael <raph.koh@gmail.com>
2017-02-28 17:52:52 -0500

    Add controls to main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # BEAM
-rho = 265.e-10              # resistivity of the beam (Ohm-meter)
+rho = 265.e-10              # resistivity of the beam (Ohm-meter) 265
 mu_0 = 4.e-7 * np.pi        # vacuum permeability (N/A^2)
 h_0 = 25.e-3                # thickness of beam (m); first and last 61m of track
 h = 15.e-3                  # thickness of beam (m); rest of track
@@ -25,9 +25,13 @@ rpm_to_angular_vel = lambda rpm: 30 * rpm / np.pi
 tangential_vel = lambda angular_vel: r * angular_vel    # tangential velocity of wheel given
 rpm_to_tan_vel = lambda rpm: tangential_vel(rpm_to_angular_vel(rpm))
 
+# FORCES (v_relative -> Force)
+F_lift_v = lambda v: ((3 * mu_0 * m ** 2) / (32 * np.pi * z_0 ** 4)) * (1 - (w / np.sqrt(v ** 2 + w ** 2)))
+F_drag_v = lambda v: (w / v) * F_lift_v(v)
+
 # FORCES (RPM -> Force)
-F_lift = lambda rpm: ((3 * mu_0 * m ** 2) / (32 * np.pi * z_0 ** 4)) * (1 - (w / np.sqrt(rpm_to_tan_vel(rpm) ** 2 + w ** 2)))
-F_drag = lambda rpm: (w / rpm_to_tan_vel(rpm)) * F_lift(rpm)
+F_lift_rpm = lambda rpm: ((3 * mu_0 * m ** 2) / (32 * np.pi * z_0 ** 4)) * (1 - (w / np.sqrt(rpm_to_tan_vel(rpm) ** 2 + w ** 2)))
+F_drag_rpm = lambda rpm: (w / rpm_to_tan_vel(rpm)) * F_lift_rpm(rpm)
 
 
 def Newton(f, x0, tolerance, derivative=False):
@@ -54,28 +58,41 @@ def getMax(f):
     x0 = 3
     return Newton(f, x0, tolerance, True)
 
-# Compute the x and y coordinates
+
+# CONTROLS
+display_points = False  # display POI and max/min
+F_var = "v"             # v or rpm
 START = 0.001
-END = 5
+END = 25
 STEP = 0.1
+
+# Compute the x and y coordinates
 x = np.arange(START, END, STEP)
+if F_var == "v":
+    F_drag = F_lift_v
+    F_lift = F_drag_v
+else:
+    F_drag = F_lift_rpm
+    F_lift = F_drag_rpm
 F_lift_plot = F_lift(x)
 F_drag_plot = F_drag(x)
-max_drag_x = getMax(F_drag)
-max_drag_y = F_drag(max_drag_x)
-POI_x = getPOI(F_lift, F_drag)
-POI_y = F_drag(POI_x)
+if display_points:
+    max_drag_x = getMax(F_drag)
+    max_drag_y = F_drag(max_drag_x)
+    POI_x = getPOI(F_lift, F_drag)
+    POI_y = F_drag(POI_x)
 
 # Plot the points using matplotlib
 plt.plot(x, F_lift_plot)
 plt.plot(x, F_drag_plot)
-plt.plot(POI_x, POI_y, 'ro')
-plt.plot(max_drag_x, max_drag_y, 'bD')
-plt.xlabel('RPM (cycles/min)')
+plt.xlabel('RPM (cycles/min)' if F_var == "rpm" else 'V (m/s)')
 plt.ylabel('Force (N)')
-plt.title('Force vs RPM')
+plt.title('Force vs RPM' if F_var == "rpm" else 'Force vs V')
 plt.legend(['Lift Force', 'Drag Force'])
-plt.annotate('POI = (' + str('{0:.2f}'.format(POI_x)) + ', ' + str('{0:.2f}'.format(POI_y)) + ')', xy=(2, 1), xytext=(0.7, POI_y))
-plt.annotate('Max F_drag = (' + str('{0:.2f}'.format(max_drag_x)) + ', ' + str('{0:.2f}'.format(max_drag_y)) + ')',
-             xy=(2, 1), xytext=(max_drag_x - 0.2, max_drag_y + 1000))
+if display_points:
+    plt.plot(POI_x, POI_y, 'ro')
+    plt.plot(max_drag_x, max_drag_y, 'bD')
+    plt.annotate('POI = (' + str('{0:.2f}'.format(POI_x)) + ', ' + str('{0:.2f}'.format(POI_y)) + ')', xy=(2, 1), xytext=(0.7, POI_y))
+    plt.annotate('Max F_drag = (' + str('{0:.2f}'.format(max_drag_x)) + ', ' + str('{0:.2f}'.format(max_drag_y)) + ')',
+                 xy=(2, 1), xytext=(max_drag_x - 0.2, max_drag_y + 1000))
 plt.show()
