a875973802bbeab8577bcd04c78941565c29bf4e
raphael <raph.koh@gmail.com>
2017-03-06 08:28:34 -0500

    Modified value of Magnetization

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # BEAM
-rho = 265.e-10              # resistivity of the beam (Ohm-meter) 265
+rho = 265.e-10              # resistivity of the beam (Ohm-meter)
 mu_0 = 4.e-7 * np.pi        # vacuum permeability (N/A^2)
 h_0 = 25.e-3                # thickness of beam (m); first and last 61m of track
 h = 15.e-3                  # thickness of beam (m); rest of track
@@ -13,12 +13,12 @@ num_magnets = sets * 4      # total number of magnets
 spacing = 0.0127            # space between each magnet (m)
 side_len = 0.0254           # side length of each magnet (m)
 V = side_len ** 3           # volume of neodymium magnets (m^3)
-M = 7580.e3                 # magnetization of neodymium magnets (A/m)
+M = 1138.e3                 # magnetization of neodymium magnets (A/m)
 m = V * M                   # magnetic vertical dipole moment (A-m^2)
 w = 2 * rho / (mu_0 * h)    # velocity of magnetic propagation through beam (m/s)
 # WHEEL
 r = ((side_len + spacing) * num_magnets) / (2 * np.pi)  # radius of wheel
-
+print (r)
 
 # CONVERSIONS
 rpm_to_angular_vel = lambda rpm: 30 * rpm / np.pi
